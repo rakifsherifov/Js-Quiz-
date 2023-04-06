7a9cc311fe5503ffa6ab325a4a47920e41705314
Rakif R. Sharifov <rakifrsh@code.local>
2023-04-06 12:45:13 +0400

    elave edildi

---
+++ assets/js/scritp.js
@@ -0,0 +1,23 @@
+// document.querySelector("button"){
+
+// }
+
+
+document.querySelector("input"){
+    Math.random("input")
+}
+// let reqem = new reqem;
+
+const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const number = ["1", "2", "3","4","5","6","7","8","9","0"]
+function Random(random) {
+    for(i=0; i>input.value; i++){
+        return number;
+    }
+    for(i=A; i>input.value; i++){
+        return alphabet;
+    }
+
+}
+
+
